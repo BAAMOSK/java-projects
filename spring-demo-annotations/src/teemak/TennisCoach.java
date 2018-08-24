56f5af0c722ee79c81a145493b35a2c46f8ceaf3
baamosk <bunth667@gmail.com>
2018-08-24 09:07:52 -0400

    scope annotation

--- spring-demo-annotations/src/teemak/TennisCoach.java
+++ spring-demo-annotations/src/teemak/TennisCoach.java
@@ -1,22 +1,26 @@
 package teemak;
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 
 @Component
+@Scope("prototype")
 public class TennisCoach implements Coach {
-	
+	@Autowired
+	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
-	
+		
 	public TennisCoach() {
 		System.out.println(">> inside constructor method");
 	}
 	
 	//SETTER method
-	@Autowired
-	public void setFortuneService(FortuneService theFortuneService) {
-		System.out.println(">> inside setFortuneService setter method");
+	/*@Autowired
+	public void doSomeCrazyStuff(FortuneService theFortuneService) {
+		System.out.println(">> inside doSomeCrazyStuff setter method");
 		fortuneService = theFortuneService;		
-	}
+	}*/
 	/*Constructor
 	@Autowired
 	public TennisCoach(FortuneService theFortuneService) {
